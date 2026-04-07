3ea404c2a560ea7b10e5bbaa2683760af89a0d2a
Fahim Habib <fams.workstation@gmail.com>
2026-04-07 23:52:08 +0600

    Varriable Added

--- Varriable&DataType.py
+++ Varriable&DataType.py
@@ -1,3 +1,23 @@
 #This is a comment
 print("Hello, World!")
 
+#Python Variables
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = 4       # x is of type int
+# x = "Sally" # x is now of type str
+# print(x)
+
+# Casting
+# If you want to specify the data type of a variable,
+# this can be done with casting.
+
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+# Case-Sensitive
+# Variable names are case-sensitive.
