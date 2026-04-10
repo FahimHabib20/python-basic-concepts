6308adc788f4e9354518c477f4a71ba4a4f5a416
Fahim Habib <fams.workstation@gmail.com>
2026-04-10 11:01:49 +0600

    day 1 basic

---
+++ day1/helloworld.py
@@ -0,0 +1,20 @@
+print("hello world")
+
+print(2 + 3) #addition(+)
+print(4 - 3) #subtraction(-)
+print(4 * 3) #multiplication(*)
+print(4 / 3) #division(/)
+print(4 % 3) #modulus(%)
+print(4 ** 3) #exponent(**)
+print(4 // 3) #floor division operator (//)
+
+# Checking data types
+
+print(type(10))  # Int
+print(type(3.14)) # Float
+print(type("Hello")) # String
+print(type(True)) # Boolean
+print(type([1, 2, 3])) # List
+print(type({'name': 'Asabeneh'}))# Dictionary
+print(type({9.8, 3.14, 2.7}))# Tuple
+print(type(1 + 3j)) # Complex
